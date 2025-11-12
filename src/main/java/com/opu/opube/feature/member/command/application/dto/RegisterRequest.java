3c1c75f43caf87aad94464ba6a2d00aa881610cb
jinyoung <kimjy3520@gmail.com>
2025-11-12 23:29:13 +0900

    :sparkles:fear: 회원가입용 Request, Response DTO 생성

---
+++ src/main/java/com/opu/opube/feature/member/command/application/dto/RegisterRequest.java
@@ -0,0 +1,20 @@
+package com.opu.opube.feature.member.command.application.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class RegisterRequest {
+    @Email @NotBlank
+    private String email;
+
+    @NotBlank @Size(min = 8, max = 128)
+    private String password;
+
+    @NotBlank @Size(min = 2, max = 50)
+    private String nickname;
+
+    // 프로필 이미지 추가
+}
