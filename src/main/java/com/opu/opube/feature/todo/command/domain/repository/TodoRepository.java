173d2d49e931e4803da6fb6e22d309f14c56a222
jinyoung <kimjy3520@gmail.com>
2025-11-12 17:32:57 +0900

    :art:style: NotificationType Entity 생성

--- src/main/java/com/opu/opube/feature/todo/command/domain/repository/TodoRepository.java
+++ src/main/java/com/opu/opube/feature/todo/command/domain/repository/TodoRepository.java
@@ -1,12 +1,7 @@
 package com.opu.opube.feature.todo.command.domain.repository;
 
 import com.opu.opube.feature.todo.command.domain.aggregate.Todo;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.time.LocalDate;
-import java.util.List;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 }
