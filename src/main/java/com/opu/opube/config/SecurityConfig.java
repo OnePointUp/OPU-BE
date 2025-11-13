1c2a1caa4256eaa19ab84c4081bd796ccd1a9cf5
jinyoung <kimjy3520@gmail.com>
2025-11-13 19:36:47 +0900

    :sparkles:feat: 디버그용 SecurityConfig 수정

--- src/main/java/com/opu/opube/config/SecurityConfig.java
+++ src/main/java/com/opu/opube/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/api/v1/debug/**").permitAll()
                         .requestMatchers(HttpMethod.OPTIONS).permitAll()
                         .anyRequest().authenticated()
                 );
