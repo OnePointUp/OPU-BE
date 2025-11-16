cd744957d7906a6e4c81eb092a0bb28a442596ad
jinyoung <kimjy3520@gmail.com>
2025-11-17 01:53:40 +0900

    :sparkles:feat: MemberCommandService에서 프로필 업데이트 하는 서비스 메서드 구현

--- src/main/java/com/opu/opube/feature/member/command/application/service/MemberCommandServiceImpl.java
+++ src/main/java/com/opu/opube/feature/member/command/application/service/MemberCommandServiceImpl.java
@@ -1,15 +1,33 @@
 package com.opu.opube.feature.member.command.application.service;
 
+import com.opu.opube.exception.BusinessException;
+import com.opu.opube.exception.ErrorCode;
+import com.opu.opube.feature.member.command.application.dto.request.UpdateMemberProfileRequest;
+import com.opu.opube.feature.member.command.application.dto.response.MemberProfileResponse;
+import com.opu.opube.feature.member.command.domain.aggregate.Member;
+import com.opu.opube.feature.member.command.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import com.opu.opube.feature.member.command.domain.repository.MemberRepository;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class MemberCommandServiceImpl implements MemberCommandService {
 
     private final MemberRepository memberRepository;
 
+    @Override
+    @Transactional
+    public MemberProfileResponse updateProfile(Long memberId, UpdateMemberProfileRequest req) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND));
+
+        member.updateProfile(
+                req.getNickname(),
+                req.getBio(),
+                req.getProfileImageUrl()
+        );
+
+        return MemberProfileResponse.from(member);
+    }
 }
