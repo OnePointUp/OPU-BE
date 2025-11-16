7b1e6773416e9f0414406a6c9726ecda83d09563
jinyoung <kimjy3520@gmail.com>
2025-11-16 15:11:13 +0900

    :sparkles:feat: Presigned url 발급 DTO 추가

---
+++ src/main/java/com/opu/opube/common/s3/S3Config.java
@@ -0,0 +1,25 @@
+package com.opu.opube.common.s3;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@Configuration
+@RequiredArgsConstructor
+public class S3Config {
+
+    @Value("${aws.s3.region}")
+    private String region;
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .region(Region.of(region))
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+}
