77a9cf4c4162ea48a7c7b963a4b6bacc82ac3c6d
sezeme <stellaa223@gmail.com>
2025-11-17 00:10:39 +0900

    :fire: remove: notifiaction 도메인 폴더 삭제

--- src/main/java/com/opu/opube/feature/notifiaction/command/infrastructure/repository/JpaNotifiactionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.opu.opube.feature.notifiaction.command.infrastructure.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.opu.opube.feature.notifiaction.command.domain.aggregate.Notifiaction;
-import com.opu.opube.feature.notifiaction.command.domain.repository.NotifiactionRepository;
-
-public interface JpaNotifiactionRepository extends NotifiactionRepository, JpaRepository<Notifiaction, Long> {
-}
