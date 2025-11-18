50c5cc6c8307ec5a2e06de160b24c74038659848
jinyoung <kimjy3520@gmail.com>
2025-11-18 18:00:25 +0900

    :sparkles:feat: 비밀번호 재설정 링크 발급과 비밀번호 재설정을 위한 DTO 추가

---
+++ src/main/java/com/opu/opube/feature/auth/command/application/dto/request/PasswordResetConfirmRequest.java
@@ -0,0 +1,12 @@
+package com.opu.opube.feature.auth.command.application.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PasswordResetConfirmRequest {
+
+    private String token;
+    private String newPassword;
+}
