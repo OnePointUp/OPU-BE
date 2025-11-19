d1114637b29a5376d5ab78a88d39b0f16847487a
jinyoung <kimjy3520@gmail.com>
2025-11-19 11:20:17 +0900

    :sparkles:feat: response dto 수정

--- src/main/java/com/opu/opube/feature/notification/command/application/dto/response/NotificationResponse.java
+++ src/main/java/com/opu/opube/feature/notification/command/application/dto/response/NotificationResponse.java
@@ -1,14 +1,19 @@
 package com.opu.opube.feature.notification.command.application.dto.response;
 
 import com.opu.opube.feature.notification.command.domain.aggregate.Notification;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class NotificationResponse {
+
     private Long id;
     private String code;
     private String title;
@@ -16,6 +21,7 @@ public class NotificationResponse {
     private Integer linkedContentId;
     private Boolean read;
     private LocalDateTime createdAt;
+
 
     public static NotificationResponse from(Notification n) {
         return NotificationResponse.builder()
