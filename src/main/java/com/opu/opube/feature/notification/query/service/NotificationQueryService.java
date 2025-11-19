20842d8833b426676f18db1e3d493563a27f6f08
jinyoung <kimjy3520@gmail.com>
2025-11-19 11:20:09 +0900

    :sparkles:feat: 알림 목록 조회, 안읽음 알림 갯수 조회하는 서비스 메서드 구현

--- src/main/java/com/opu/opube/feature/notification/query/service/NotificationQueryService.java
+++ src/main/java/com/opu/opube/feature/notification/query/service/NotificationQueryService.java
@@ -1,15 +1,36 @@
 package com.opu.opube.feature.notification.query.service;
 
+import com.opu.opube.common.dto.PageResponse;
+import com.opu.opube.feature.notification.command.application.dto.response.NotificationResponse;
+import com.opu.opube.feature.notification.query.infrastructure.repository.NotificationQueryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import com.opu.opube.feature.notification.query.infrastructure.repository.NotificationQueryRepository;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class NotificationQueryService {
 
     private final NotificationQueryRepository notificationQueryRepository;
 
+
+    @Transactional(readOnly = true)
+    public PageResponse<NotificationResponse> getMyNotifications(
+            Long memberId,
+            boolean onlyUnread,
+            Pageable pageable
+    ) {
+        Page<NotificationResponse> page =
+                notificationQueryRepository.findMyNotifications(memberId, onlyUnread, pageable);
+
+        return PageResponse.from(page);
+    }
+
+
+    @Transactional(readOnly = true)
+    public long getUnreadCount(Long memberId) {
+        return notificationQueryRepository.countUnread(memberId);
+    }
 }
