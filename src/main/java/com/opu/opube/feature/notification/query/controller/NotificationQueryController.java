30b68447010399552a50ae506d9e415b495ae2b9
jinyoung <kimjy3520@gmail.com>
2025-11-19 11:20:37 +0900

    :sparkles:feat: 알림 목록 조회, 안읽은 알림 갯수 조회하는 엔드포인트 추가

--- src/main/java/com/opu/opube/feature/notification/query/controller/NotificationQueryController.java
+++ src/main/java/com/opu/opube/feature/notification/query/controller/NotificationQueryController.java
@@ -1,14 +1,47 @@
 package com.opu.opube.feature.notification.query.controller;
 
+import com.opu.opube.common.dto.ApiResponse;
+import com.opu.opube.common.dto.PageResponse;
+import com.opu.opube.feature.auth.command.application.security.MemberPrincipal;
+import com.opu.opube.feature.notification.command.application.dto.response.NotificationResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import com.opu.opube.feature.notification.query.service.NotificationQueryService;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/notification")
+@RequestMapping("/api/v1/notifications")
 public class NotificationQueryController {
 
     private final NotificationQueryService notificationQueryService;
 
+    @GetMapping
+    public ApiResponse<PageResponse<NotificationResponse>> getMyNotifications(
+            @AuthenticationPrincipal MemberPrincipal principal,
+            @RequestParam(defaultValue = "false") boolean onlyUnread,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size
+    ) {
+        Long memberId = principal.getMemberId();
+        PageRequest pageable = PageRequest.of(page, size);
+
+        PageResponse<NotificationResponse> res =
+                notificationQueryService.getMyNotifications(memberId, onlyUnread, pageable);
+
+        return ApiResponse.success(res);
+    }
+
+    @GetMapping("/unread-count")
+    public ApiResponse<Long> getUnreadCount(
+            @AuthenticationPrincipal MemberPrincipal principal
+    ) {
+        long count = notificationQueryService.getUnreadCount(principal.getMemberId());
+        return ApiResponse.success(count);
+    }
 }
