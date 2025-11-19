32bb82f9e03eba4f20d782992b77d021ce95a2ed
jinyoung <kimjy3520@gmail.com>
2025-11-19 11:22:25 +0900

    :art:style: 사용하지 않는 import문 제거

--- src/main/java/com/opu/opube/feature/notification/command/domain/repository/NotificationRepository.java
+++ src/main/java/com/opu/opube/feature/notification/command/domain/repository/NotificationRepository.java
@@ -2,8 +2,6 @@ package com.opu.opube.feature.notification.command.domain.repository;
 
 import com.opu.opube.feature.notification.command.domain.aggregate.Notification;
 import io.lettuce.core.dynamic.annotation.Param;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
