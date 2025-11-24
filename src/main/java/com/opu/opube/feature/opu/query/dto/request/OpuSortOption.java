1fd31c2a3cf2ff9c06a42293ae4fac86f2dd0975
jinyoung <kimjy3520@gmail.com>
2025-11-25 02:44:21 +0900

    :sparkles:feat: OpuSortOption enum 클래스 생성

---
+++ src/main/java/com/opu/opube/feature/opu/query/dto/request/OpuSortOption.java
@@ -0,0 +1,31 @@
+package com.opu.opube.feature.opu.query.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public enum OpuSortOption {
+    NEWEST("newest"),
+    NAME_ASC("name_asc"),
+    COMPLETION("completion"),
+    FAVORITE("favorite");
+
+    private final String code;
+
+    OpuSortOption(String code) {
+        this.code = code;
+    }
+
+    public static OpuSortOption from(String code) {
+        if (code == null || code.isBlank()) {
+            return NEWEST;
+        }
+
+        for (OpuSortOption option : values()) {
+            if (option.code.equalsIgnoreCase(code)) {
+                return option;
+            }
+        }
+
+        return NEWEST;
+    }
+}
