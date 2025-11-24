48439aeaba5dd04fdcd3a7ba03f4420351fdc902
jinyoung <kimjy3520@gmail.com>
2025-11-25 00:55:43 +0900

    :sparkles:feat: 사용하지 않는 import문 제거

--- src/main/java/com/opu/opube/feature/opu/query/infrastructure/repository/OpuQueryRepository.java
+++ src/main/java/com/opu/opube/feature/opu/query/infrastructure/repository/OpuQueryRepository.java
@@ -1,13 +1,11 @@
 package com.opu.opube.feature.opu.query.infrastructure.repository;
 
-import java.util.List;
 import java.util.Optional;
 
 import com.opu.opube.common.dto.PageResponse;
 import com.opu.opube.feature.opu.command.domain.aggregate.Opu;
 import com.opu.opube.feature.opu.query.dto.request.OpuListFilterRequest;
 import com.opu.opube.feature.opu.query.dto.response.OpuSummaryResponse;
-import org.springframework.data.domain.Pageable;
 
 public interface OpuQueryRepository {
     // 좋아요 한 OPU수
