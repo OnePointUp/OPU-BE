884c2515a64d0737318d6d2e166c76b9a0bf6861
sezeme <stellaa223@gmail.com>
2025-12-03 23:43:55 +0900

    :sparkles: feat: dto에 getter 추가

--- src/main/java/com/opu/opube/feature/todo/query/dto/response/RoutineListResponseDto.java
+++ src/main/java/com/opu/opube/feature/todo/query/dto/response/RoutineListResponseDto.java
@@ -3,10 +3,12 @@ package com.opu.opube.feature.todo.query.dto.response;
 import com.opu.opube.feature.todo.command.domain.aggregate.Frequency;
 import com.opu.opube.feature.todo.command.domain.aggregate.Routine;
 import lombok.Builder;
+import lombok.Getter;
 
 import java.time.LocalDate;
 
 @Builder
+@Getter
 public class RoutineListResponseDto {
     Long id;
     LocalDate startDate;
