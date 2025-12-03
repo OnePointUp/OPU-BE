9c2251ee7639311239eebbe28988717c0562834c
jinyoung <kimjy3520@gmail.com>
2025-12-03 15:08:14 +0900

    :sparkles:feat: AuthProvider 상수 클래스 추가

---
+++ src/main/java/com/opu/opube/feature/member/command/domain/aggregate/AuthProvider.java
@@ -0,0 +1,8 @@
+package com.opu.opube.feature.member.command.domain.aggregate;
+
+public final class AuthProvider {
+    public static final String KAKAO = "kakao";
+    public static final String LOCAL = "local";
+
+    private AuthProvider() {}
+}
