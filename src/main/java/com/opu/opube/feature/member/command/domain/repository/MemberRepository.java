80b405e6b6c0ab38bf1ebfd43651d8abb6a7c9da
jinyoung <kimjy3520@gmail.com>
2025-12-11 11:28:16 +0900

    :recycle:refactor: MemberRepository 도메인 인터페이스와 JpaMemberRepository로 분리

--- src/main/java/com/opu/opube/feature/member/command/domain/repository/MemberRepository.java
+++ src/main/java/com/opu/opube/feature/member/command/domain/repository/MemberRepository.java
@@ -1,15 +1,14 @@
 package com.opu.opube.feature.member.command.domain.repository;
 
 import com.opu.opube.feature.member.command.domain.aggregate.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository {
+    Optional<Member> findById(Long id);
     Optional<Member> findByEmail(String email);
     Optional<Member> findByAuthProviderAndProviderId(String authProvider, String providerId);
     boolean existsByEmail(String email);
-    boolean existsByNicknameAndNicknameTag(String nickname, String nicknameTag); // 추가
+    boolean existsByNicknameAndNicknameTag(String nickname, String nicknameTag);
+    Member save(Member member);
 }
--- src/main/java/com/opu/opube/feature/member/command/infrastructure/repository/JpaMemberRepository.java
+++ src/main/java/com/opu/opube/feature/member/command/infrastructure/repository/JpaMemberRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.opu.opube.feature.member.command.domain.aggregate.Member;
 import com.opu.opube.feature.member.command.domain.repository.MemberRepository;
 
-public interface JpaMemberRepository extends MemberRepository, JpaRepository<Member, Long> {
+public interface JpaMemberRepository
+        extends MemberRepository, JpaRepository<Member, Long> {
 }
