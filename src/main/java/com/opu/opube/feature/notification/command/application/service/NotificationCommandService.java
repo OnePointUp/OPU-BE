7c1aee8177421ad71e3f791372bedf07a60de6de
jinyoung <kimjy3520@gmail.com>
2025-12-12 14:03:50 +0900

    :recycle:refactor: sendNotification return 타입 수정

--- src/main/java/com/opu/opube/feature/notification/command/application/service/NotificationCommandService.java
+++ src/main/java/com/opu/opube/feature/notification/command/application/service/NotificationCommandService.java
@@ -1,11 +1,10 @@
 package com.opu.opube.feature.notification.command.application.service;
 
-import com.opu.opube.feature.notification.command.application.dto.response.NotificationResponse;
 import com.opu.opube.feature.notification.command.domain.aggregate.NotificationTypeCode;
 
 public interface NotificationCommandService {
 
-    NotificationResponse sendNotification(
+    void sendNotification(
             Long memberId,
             NotificationTypeCode typeCode,
             String title,
